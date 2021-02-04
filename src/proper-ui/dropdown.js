d335396de1565e959230c0e5e6e558c928e0a5de
YasH <yashdesoi@gmail.com>
2021-02-04 14:05:06 +0530

    Added a smooth transition to dropdown component

--- src/proper-ui/dropdown.js
+++ src/proper-ui/dropdown.js
@@ -5,12 +5,21 @@ class Dropdown {
         this.container = container;
         this.trigger = container.querySelector('.trigger');
         this.content = container.querySelector('.content');
+        // Storing the value of the height height before setting it to 0;
+        this.trueContentHeight = this.content.offsetHeight;
+        this.content.style.height = 0;
+        this.clicks = 0;
     }
 
     init() {
-        this.container.addEventListener('click', () => {
+        this.trigger.addEventListener('click', () => {
+            this.clicks += 1;
             this.trigger.classList.toggle('active');
-            this.content.classList.toggle('active');
+            if (this.clicks%2 === 1) {
+                this.content.style.height = `${this.trueContentHeight}px`;
+            } else {
+                this.content.style.height = '0px';
+            }
         });
     }
 }
